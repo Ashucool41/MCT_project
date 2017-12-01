24f6e4ee06bdd459fd9374aa554e90e276ed9263
MANOJ V <v.manoj1996@gmail.com>
2017-12-02 04:15:27 +0530

    mpc code is written

--- quadcopterMPC/fetchModel.m
+++ quadcopterMPC/fetchModel.m
@@ -3,9 +3,10 @@ syms A B Xk Xk_prev Uk Uk_prev
 syms x y z vx vy vz  wx wy wz phi theta psi 
 X_syms = [x; y; z; vx; vy; vz;  wx; wy; wz; phi; theta; psi];
 %% MODEL PARAMETERS
-Cth =0.75*9.8;%Newton/unit throttleInput 750gm thrust C2830 motor+10X4.5 inch propeller(http://www.rcbazaar.com/product.aspx?productid=1905)
-%Note that all inputs(to esc--pulse width(uS)) mentioned here are scaled between 0.0-1.0 for
-%conveniance sake
+Cth =0.75*9.8;%Newton/unit throttleInput 750gm thrust C2830 motor+10X4.5 
+%inch propeller(http://www.rcbazaar.com/product.aspx?productid=1905)
+%Note that all inputs(to esc--pulse width(uS)) mentioned here are scaled 
+%between 0.0-1.0 for conveniance sake
 L =0.45/2; %(half length of quad's diagonal)
 torqByThrust= 0.0162;%(Obtained from UIUC Propeller database)
 
@@ -13,7 +14,8 @@ mass = 1.5;
 Ixx=0.0142;
 Iyy=Ixx;
 Izz=0.0284;
-u0=0.56;%Average Control input given to each ESC(Electronic speed Controller) at hover condition(taken from pixhawk log)
+u0=0.56;%Average Control input given to each ESC(Electronic speed 
+%Controller) at hover condition(taken from pixhawk log)
 
 %% Linearised Model about Hover condition for quadcopter
 %X_dot=A_l*x+B_l*u+C    Model is in this form
@@ -47,7 +49,9 @@ B_l;
 
 CnstBias=[0 0 0 0 0 9.81 0 0 0 0 0 0]'; %Constant offset in the model
 
-Ts=0.02; %Time for Each MPC Iteration. Since 20mS is usually used for regular PID based controllers in quadcopters. the same number is chosen for MPC also
+Ts=0.02; %Time for Each MPC Iteration. Since 20mS is usually used for 
+%regular PID based controllers in quadcopters. the same number is chosen
+%for MPC also
 Ad=expm(A_l*Ts)
 syms tau
 temp1=expm(A_l*tau);
